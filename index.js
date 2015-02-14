e2672c603f7de382033dd07e4382707d39f329ae
Aaron Krause <aaronjkrause@gmail.com>
2015-02-13 19:53:15 -0800

    router functional, gruntfile added and running jshint, jscs

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 var server = require('./lib/server.js');
+var router = require('./lib/router.js');
 
+router.get('/', function(req, res){
+  res.writeHead(200, {'Content-Type':'text/plain'});
+  res.end('MADE IT FUCKER');
+})
+router.get('/holyshit', function(req, res){
+  res.writeHead(200, {'Content-Type':'text/plain'});
+  res.end('HOLY SHHIIIIIIT');
+})
 server.start(3000);
