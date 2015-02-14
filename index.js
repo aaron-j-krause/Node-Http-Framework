29c5f19d107e92a6801dfb846c1e742cc45fbb28
Aaron Krause <aaronjkrause@gmail.com>
2015-02-13 19:58:20 -0800

    cleanup

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var server = require('./lib/server.js');
 var router = require('./lib/router.js');
 
-router.get('/', function(req, res){
+router.get('/', function(req, res) {
   res.writeHead(200, {'Content-Type':'text/plain'});
   res.end('MADE IT FUCKER');
-})
-router.get('/holyshit', function(req, res){
+});
+
+router.get('/holyshit', function(req, res) {
   res.writeHead(200, {'Content-Type':'text/plain'});
   res.end('HOLY SHHIIIIIIT');
-})
+});
+
 server.start(3000);
