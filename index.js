e9f31a5eb58b818b31239b342ac67a2b514dcb5c
Aaron Krause <aaronjkrause@gmail.com>
2015-02-14 18:26:36 -0800

    helper methods working

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 var server = require('./lib/server.js');
 var router = require('./lib/router.js');
+var fs = require('fs');
 
 router.get('/', function(req, res) {
-  res.writeHead(200, {'Content-Type':'text/plain'});
-  res.end('MADE IT FUCKER');
+  req.on('end', function(){
+    res.writeHead(200, {'Content-Type':'text/plain'});
+    res.end('MADE IT FUCKER');
+  })
 });
 
-router.get('/holyshit', function(req, res) {
-  res.writeHead(200, {'Content-Type':'text/plain'});
-  res.end('HOLY SHHIIIIIIT');
+router.get('/waow', function(req, res) {
+  res.send('waow');
 });
 
+router.post('/', function(req, res) {
+  req.on('end', function(){
+    var filePath = './data/test.json';
+    var info = JSON.stringify(req.body);
+    fs.writeFile('./data/test.json', info, function(err){
+      if (err) throw err;
+      res.writeHead(200, {'Content-Type': 'application/json'})
+      res.end(info);
+    })
+  })
+})
+router.put('/data/test', function(req, res) {
+})
 server.start(3000);
