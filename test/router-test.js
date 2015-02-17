35d395690a987abd22b59a10e5b43f38b2e0081e
Aaron Krause <aaronjkrause@gmail.com>
2015-02-16 16:47:39 -0800

    server test working, all tests functional

--- test/router-test.js
+++ test/router-test.js
@@ -6,40 +6,56 @@ chai.use(chaihttp);
 
 describe('Router', function() {
   describe('route setters', function() {
+    after(function() {
+      router.resetRouter();
+    });
+
     it('should assign a route to the route object for GET', function() {
       router.get('/test', function(req, res) {});
       var routes = router.showRoutes();
       expect(routes['/test'].GET).to.be.a('function');
     });
+
     it('should assign a route to the route object for POST', function() {
       router.post('/test', function(req, res) {});
       var routes = router.showRoutes();
       expect(routes['/test'].POST).to.be.a('function');
     });
+
     it('should assign a route to the route object for PUT', function() {
       router.put('/test', function(req, res) {});
       var routes = router.showRoutes();
       expect(routes['/test'].PUT).to.be.a('function');
     });
+
     it('should assign a route to the route object for PATCH', function() {
       router.patch('/test', function(req, res) {});
       var routes = router.showRoutes();
       expect(routes['/test'].PATCH).to.be.a('function');
     });
+
     it('should assign a route to the route object for DELETE', function() {
       router.del('/test', function(req, res) {});
       var routes = router.showRoutes();
       expect(routes['/test'].DELETE).to.be.a('function');
     });
+
   });
   describe('route function', function() {
+
+    after(function() {
+      router.resetRouter();
+    });
+
     it('should call stored function', function() {
       var results;
       router.get('/lasttest', function() {
         results = true;
       });
+
       router.route('/lasttest', 'GET', 'req', 'res');
       expect(results).to.eql(true);
     });
+
   });
 });
--- test/server-test.js
+++ test/server-test.js
@@ -1,7 +1,42 @@
 var chai = require('chai');
 var chaihttp = require('chai-http');
+var fs = require('fs');
 var expect = chai.expect;
 chai.use(chaihttp);
-var router = require('../lib/router');
+var server = require('../lib/server.js');
 
-var server = require('../lib/server');
+describe('Server', function() {
+
+  before(function() {
+    server.setFileDestination('/test', 'TestDirForTests');
+    server.start(3333);
+  });
+
+  after(function() {
+    fs.rmdirSync(__dirname + '/../TestDirForTests');
+    server.resetOptions();
+    server.stop();
+  });
+
+  it('should start a server', function(done) {
+    chai.request('localhost:3333/')
+        .get('/test')
+        .end(function(err, data) {
+          expect(data).to.have.status(404);
+          done();
+        });
+  });
+
+  it('should create a directory', function(done) {
+    fs.readdir(__dirname + '/../', function(err, data) {
+      expect(data).to.contain('TestDirForTests');
+      done();
+    });
+  });
+
+  it('should add path to paths', function() {
+    var results = server.returnOptions();
+    expect(results).to.have.property('/test');
+  });
+
+});
